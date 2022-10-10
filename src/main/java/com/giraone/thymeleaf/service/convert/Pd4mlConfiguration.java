b2e8d5b83d7c35e5c4d05b9c2ad988c30f75a90a
Hans Schuell <hans.schuell@gmx.de>
2022-10-10 16:00:49 +0200

    Set default PD4ML log level back to 5.

--- src/main/java/com/giraone/thymeleaf/service/convert/Pd4mlConfiguration.java
+++ src/main/java/com/giraone/thymeleaf/service/convert/Pd4mlConfiguration.java
@@ -3,7 +3,7 @@ package com.giraone.thymeleaf.service.convert;
 public class Pd4mlConfiguration {
 
     /** Log level for PD4ML. Default = 5. Higher number means more detailed logs. */
-    private int logLevel = 10;
+    private int logLevel = 5;
 
     public Pd4mlConfiguration() {
     }

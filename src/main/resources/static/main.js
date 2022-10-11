6981fba67bb60e522c86a2b5228f1a1b0cbb81f4
Hans Schuell <hans.schuell@gmx.de>
2022-10-11 22:43:22 +0200

    More and better samples

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,47 +1,91 @@
-function test1() {
-   const jsonContent = '{"name": "User"}';
-   const templateContent = '<div>Hello&nbsp;<span th:text="${name}">World</span>!</div>';
-   const cssContent = '';
-   const expected = 'Hello World';
-   multipartUpload('/api/json-to-html', jsonContent, templateContent, cssContent, showHtml, showError, expected);
+function testJsonToHtmlSimple() {
+  const jsonContent = '{"name": "User"}';
+  const templateContent = '<div>Hello&nbsp;<span th:text="${name}">World</span>!</div>';
+  const cssContent = '';
+  const expected = 'Hello User!';
+  multipartUploadForHtml('/api/json-to-html', jsonContent, templateContent, cssContent, expected);
 }
 
-function test2() {
-   const jsonContent = '{"name": "Red"}';
-   const templateContent = '<html><head><style></style></head><body><div>Hello&nbsp;<span class="strong" th:text="${name}">World</span>!</div></body></html>';
-   const cssContent = '.strong { color: red; font-weight: bold; }';
-   const expected = 'Hello Red';
-   multipartUpload('/api/json-to-html', jsonContent, templateContent, cssContent, showHtml, showError, expected);
+function testJsonToHtmlWithCss() {
+  const jsonContent = '{"name": "Red"}';
+  const templateContent = '<html><head><style></style></head><body><div>Hello&nbsp;<span class="strong" th:text="${name}">World</span>!</div></body></html>';
+  const cssContent = '.strong { color: red; font-weight: bold; }';
+  const expected = 'Hello Red!';
+  multipartUploadForHtml('/api/json-to-html', jsonContent, templateContent, cssContent, expected);
 }
 
-function multipartUpload(url, jsonContent, templateContent, cssContent, successCallback, errorCallback, expected) {
+function testJsonToPdfWithCss() {
+  const jsonContent = '{"name": "Red"}';
+  const templateContent = '<html><head><style></style></head><body><div>Hello&nbsp;<span class="strong" th:text="${name}">World</span>!</div></body></html>';
+  const cssContent = '.strong { color: red; font-weight: bold; }';
+  multipartUploadForPdf('/api/json-to-pdf', jsonContent, templateContent, cssContent);
+}
 
-   const formData = new FormData();
-   formData.append('data', new Blob([jsonContent], { type: 'application/json;charset=UTF-8' }), 'data.json');
-   formData.append('template', new Blob([templateContent], { type: 'text/html;charset=UTF-8'}), 'template.html');
-   formData.append('css', new Blob([cssContent], { type: 'text/css;charset=UTF-8'}), 'template.css');
+function testJsonToPdfWithFonts() {
+  const jsonContent = '{"name": "Red"}';
+  const templateContent = '<html><head><style></style></head><body><div>Hello&nbsp;<span class="strong" th:text="${name}">World</span>!</div></body></html>';
+  const cssContent = '* { font-family: "Bad Script", cursive; } .strong { color: red; font-weight: bold; }';
+  multipartUploadForPdf('/api/json-to-pdf', jsonContent, templateContent, cssContent);
+}
 
-   let req = new XMLHttpRequest();
-   req.open('POST', url);
-   req.setRequestHeader('Accept', 'text/html');
+function multipartUploadForHtml(url, jsonContent, templateContent, cssContent, expected) {
 
-   req.onload = function() {
-       if (req.status == 200) {
-           showHtml(req.responseText, expected);
-       } else {
-           showError(req.status, req.statusText, req.responseText);
-       }
-   }
+  const formData = new FormData();
+  formData.append('data', new Blob([jsonContent], { type: 'application/json;charset=UTF-8' }), 'data.json');
+  formData.append('template', new Blob([templateContent], { type: 'text/html;charset=UTF-8'}), 'template.html');
+  formData.append('css', new Blob([cssContent], { type: 'text/css;charset=UTF-8'}), 'template.css');
 
-   req.send(formData);
+  const req = new XMLHttpRequest();
+  req.open('POST', url);
+  req.setRequestHeader('Accept', 'text/html');
+
+  req.onload = function() {
+    if (req.status == 200) {
+      showHtml(req.responseText, expected);
+    } else {
+      showError(req.status, req.statusText, req.responseText);
+    }
+  }
+
+  req.send(formData);
+}
+
+function multipartUploadForPdf(url, jsonContent, templateContent, cssContent, errorCallback) {
+
+  const formData = new FormData();
+  formData.append('data', new Blob([jsonContent], { type: 'application/json;charset=UTF-8' }), 'data.json');
+  formData.append('template', new Blob([templateContent], { type: 'text/html;charset=UTF-8'}), 'template.html');
+  formData.append('css', new Blob([cssContent], { type: 'text/css;charset=UTF-8'}), 'template.css');
+
+  const contentType = 'application/pdf';
+  const req = new XMLHttpRequest();
+  req.open('POST', url);
+  req.setRequestHeader('Accept', contentType);
+  req.responseType = 'arraybuffer';
+  req.onload = function() {
+    if (req.status == 200) {
+      const arrayBuffer = req.response; // Note: not req.responseText!
+      if (arrayBuffer) {
+        const blob = new Blob([req.response], {type: contentType});
+        const nextUrl = URL.createObjectURL(blob);
+        window.open(nextUrl, '_blank');
+      } else {
+         alert("No content returned from " + url);
+      }
+    } else {
+      showError(req.status, req.statusText, '');
+    }
+  };
+
+  req.send(formData);
 }
 
 function showHtml(htmlContent, expected) {
-    document.getElementById('status').textContent = 'Status: OK. Expected: ' + expected;
-    document.getElementById('htmlPreview').src = 'data:text/html;charset=iso8859-1,' + escape(htmlContent);
+  document.getElementById('status').textContent = 'Status: OK. Expected: ' + expected;
+  document.getElementById('htmlPreview').src = 'data:text/html;charset=iso8859-1,' + escape(htmlContent);
 }
 
-function showError(status, statusText, errorText, htmlContent) {
-    document.getElementById('status').textContent = 'ERROR: ' + statusText + '(' + status + ')';
-    document.getElementById('htmlPreview').src = 'data:text/html;charset=iso8859-1,' + escape(htmlContent);
+function showError(status, statusText, errorText) {
+  document.getElementById('status').textContent = 'ERROR: ' + statusText + '(' + status + ')';
+  document.getElementById('htmlPreview').src = '';
 }

bb037c295d9849eee1b6214e713177e7d791e3ca
Hans Schuell <hans.schuell@gmx.de>
2022-11-02 22:34:05 +0100

    Unused methods removed

--- src/main/java/com/giraone/thymeleaf/common/FileUtil.java
+++ src/main/java/com/giraone/thymeleaf/common/FileUtil.java
@@ -1,9 +1,7 @@
 package com.giraone.thymeleaf.common;
 
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.util.ResourceUtils;
 
@@ -13,8 +11,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -61,7 +57,7 @@ public final class FileUtil {
             }
         }
 
-        // The difficult part. We need a file URL, but we are running from a JAR file, where there ist no file system,
+        // The difficult part. We need a file URL, but we are running from a JAR file, where there is no file system,
         // There are only zipped archives. So we copy the content to a temporary file.
         final String[] prefixAndSuffix = extractPrefixAndSuffix(url.getPath());
         final String prefix = prefixAndSuffix[0];
@@ -106,149 +102,6 @@ public final class FileUtil {
         return bytes == null ? null : new String(bytes, encoding);
     }
 
-    /**
-     * Copy files from a resource path (classpath with file extension filter) to a target directory in the (local) file system.
-     * The copied files will keep the last modification date of the original file.
-     * @param resourcePath  A classpath: resource, e.g. "defaultfonts", "testdata/simple" or "testdata/". If the value ends with "/",
-     * the directory is looked up recursively.
-     * @param endsWith  An optional (maybe null) file extension, e.g. \".ttf\", \".css\", to be used as a filter.
-     * @param targetDirectory The target directory, where the files are copied to. If the target directory does not exist,
-     * it is automatically created.
-     * @param cleanIfExists A flag to clean the directory, if it already exists.
-     * @param flatten If true, the tree structure is ignored and all file are copied directly into the target directory.
-     * @return the number of copied files.
-     */
-    public static int copyResourceFiles(String resourcePath, String endsWith, File targetDirectory, boolean cleanIfExists, boolean flatten) {
-
-        String classPath;
-        if (resourcePath.endsWith("/")) {
-            classPath = resourcePath + "**/*";
-            resourcePath = resourcePath.substring(0, resourcePath.length() - 1);
-        } else {
-            classPath = resourcePath + "/*";
-        }
-        if (endsWith != null) {
-            classPath += endsWith;
-        }
-        final String matchingPath = ResourceUtils.CLASSPATH_URL_PREFIX + classPath;
-        return copyMatchingResourceFiles(matchingPath, resourcePath, targetDirectory, cleanIfExists, flatten);
-    }
-
-    /**
-     * Copy files from a resource path to a target directory in the (local) file system.
-     * The copied files will keep the last modification date of the original file.
-     * @param matchingPath A resource path resolved using {@link PathMatchingResourcePatternResolver}.
-     * @param resourceRoot The root directory of the source - Needed, if the files are deep - and not flat - copied.
-     * @param targetDirectory The target directory, where the files are copied to. If the target directory does not exist,
-     * it is automatically created.
-     * @param cleanIfExists A flag to clean the directory, if it already exists.
-     * @param flatten If true, the tree structure is ignored and all file are copied directly into the target directory.
-     * @return the number of copied files.
-     */
-    public static int copyMatchingResourceFiles(String matchingPath, String resourceRoot, File targetDirectory, boolean cleanIfExists, boolean flatten) {
-
-        if (!targetDirectory.exists()) {
-            if (!targetDirectory.mkdirs()) {
-                throw new RuntimeException("Cannot create target directory \"" + targetDirectory + "\"!");
-            }
-        } else {
-            if (cleanIfExists) {
-                try {
-                    FileUtils.cleanDirectory(targetDirectory);
-                } catch (IOException e) {
-                    throw new RuntimeException("Cannot clean target directory \"" + targetDirectory + "\"!", e);
-                }
-            }
-        }
-
-        int count = 0;
-        final Resource[] resources;
-        try {
-            resources = pathMatchingResourcePatternResolver.getResources(matchingPath);
-        } catch (IOException e) {
-            if (e.getMessage().contains("does not exist")) {
-                // Silently ignore, when there are no matching files
-                LOGGER.info("No files found in \"{}\"", matchingPath);
-                return 0;
-            }
-            throw new RuntimeException("Cannot lookup files in \"" + matchingPath + "\"!", e);
-        }
-        for (Resource resource : resources) {
-            // Directories are not readable
-            if (!resource.isReadable()) {
-                LOGGER.debug("Skipping unreadable resource \"{}\"", resource);
-                continue;
-            }
-            final String filename = resource.getFilename();
-            if (filename == null) {
-                continue;
-            }
-            try {
-                final URL url;
-                try {
-                    url = resource.getURL();
-                } catch (FileNotFoundException e) {
-                    throw new RuntimeException("Cannot get URL for resource " + resource
-                        + " file=\"" + resource.getFilename() + "\"!", e);
-                }
-
-                final File targetFile;
-                if (flatten) {
-                    targetFile = new File(targetDirectory, filename);
-                } else {
-                    final File resourceFile;
-                    try {
-                        resourceFile = new File(url.toURI().getSchemeSpecificPart());
-                    } catch (URISyntaxException e) {
-                        throw new RuntimeException(e);
-                    }
-                    final String parentPath = resourceFile.getParent().replace('\\', '/');
-                    final String targetParent = extractNeededParentPath(parentPath, resourceRoot);
-                    if (targetParent == null) {
-                        throw new RuntimeException("parentPath/resourceRoot mismatch parentPath=\"" + parentPath
-                            + "\", resourceRoot=\"" + resourceRoot + "\"!");
-                    }
-                    targetFile = new File(targetDirectory, targetParent + filename);
-                    final File targetParentFile = targetFile.getParentFile();
-                    // Automatically create subdirectories, when flatten=false
-                    if (!targetParentFile.exists()) {
-                        boolean autoCreateTargetDirectories = targetParentFile.mkdirs();
-                        if (!autoCreateTargetDirectories) {
-                            throw new RuntimeException("Cannot create target directory \"" + targetParentFile + "\"!");
-                        }
-                    }
-                }
-
-                copyUrlContentToFile(url, targetFile);
-                setLastModified(targetFile, resource.lastModified());
-            } catch (IOException e) {
-                throw new RuntimeException("Cannot copy resource file \"" + filename + "\" to \"" + targetDirectory + "\"!", e);
-            }
-            count++;
-        }
-        return count;
-    }
-
-    public static boolean fileExistsInResourceOrSrcDir(String resourcePath) {
-
-        final File srcFolder = new File(SRC_RESOURCES);
-        if (srcFolder.exists()) {
-            return new File(srcFolder.getAbsolutePath(), resourcePath).exists();
-        } else {
-            return fileExistsInResource(resourcePath);
-        }
-    }
-
-    public static boolean fileExistsInResource(String resourcePath) {
-
-        final URL url = getClassLoader().getResource(resourcePath);
-        if (url == null) {
-            return false;
-        }
-        final File file = new File(url.getFile());
-        return file.exists();
-    }
-
     public static byte[] readBytesFromUrl(URL url) throws IOException {
 
         if (url == null) {
@@ -286,18 +139,6 @@ public final class FileUtil {
 
     //------------------------------------------------------------------------------------------------------------------
 
-    static ClassLoader getClassLoader() {
-
-        // See https://docs.spring.io/spring-boot/docs/current/reference/html/appendix-executable-jar-format.html#executable-jar-restrictions
-        // return Thread.currentThread().getContextClassLoader();
-
-        // Geht nicht in Tests, die z.B. von Kommandozeile mit mvn ohne fork laufen. Ist generell keine gute Idee. (hs)
-        // return ClassLoader.getSystemClassLoader();
-
-        // Ressourcen sind in BOOT-INF - das sollte gehen (hs)
-        return FileUtil.class.getClassLoader();
-    }
-
     static String[] extractPrefixAndSuffix(String path) {
 
         int i = path.lastIndexOf('/');
@@ -330,23 +171,6 @@ public final class FileUtil {
             }
             sb.insert(0, "/").insert(0, parentSourcePath.substring(i + 1));
             parentSourcePath = parentSourcePath.substring(0, i);
-        }
-    }
-
-    static void copyUrlContentToFile(URL url, File file) throws IOException {
-        try (OutputStream out = new FileOutputStream(file)) {
-            try (InputStream in = url.openStream()) {
-                long copied = IoStreamUtil.pipeBlobStream(in, out);
-                LOGGER.debug("Copied URL \"{}\" with {} bytes to file \"{}\"", url, copied, file);
-            }
-        }
-    }
-
-    static void setLastModified(File file, long lastModified) {
-
-        boolean setLastModifiedOk = file.setLastModified(lastModified);
-        if (!setLastModifiedOk) {
-            LOGGER.warn("Cannot set modification date of file \"{}\"!", file);
         }
     }
 }
--- src/test/java/com/giraone/thymeleaf/common/FileUtilTest.java
+++ src/test/java/com/giraone/thymeleaf/common/FileUtilTest.java
@@ -1,6 +1,5 @@
 package com.giraone.thymeleaf.common;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,7 +9,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,99 +43,6 @@ class FileUtilTest {
     void givenTextFileExistsNot_whenReadTextFileFromResource_thenReturnNull() throws IOException {
         String contentAsString = FileUtil.readTextFileFromResource(NON_EXISTING_FILE, StandardCharsets.UTF_8);
         assertThat(contentAsString).isNull();
-    }
-
-    @Test
-    void givenFileExists_whenFileExistsInResource_thenReturnTrue() {
-        boolean fileExists = FileUtil.fileExistsInResource(EXISTING_FILE);
-        assertThat(fileExists).isTrue();
-    }
-
-    @Test
-    void givenFileExistsNot_whenFileExistsInResource_thenReturnFalse() {
-        boolean fileExists = FileUtil.fileExistsInResource(NON_EXISTING_FILE);
-        assertThat(fileExists).isFalse();
-    }
-
-    @Test
-    void givenDirTargetDirIsNotEmpty_whenCopyResourceFilesFlat_withoutCleanIfExists_thenTargetIsNotCleaned() throws IOException {
-        final File testDirectory = File.createTempFile("test-","");
-        // Delete temp file - we need a directory!
-        assertThat(testDirectory.delete()).isTrue();
-        assertThat(testDirectory.mkdir()).isTrue();
-
-        try {
-            final File testFile1 = File.createTempFile("test-",null, testDirectory);
-            final File testFile2 = File.createTempFile("test-",null, testDirectory);
-            int count = FileUtil.copyResourceFiles("testdata/file-util/subdir1", ".txt", testDirectory, false, true);
-            assertThat(count).isEqualTo(2);
-            assertThat(testDirectory.listFiles()).hasSize(4);
-            assertThat(testFile1.exists()).isTrue();
-            assertThat(testFile2.exists()).isTrue();
-        } finally {
-            FileUtils.deleteDirectory(testDirectory);
-        }
-    }
-
-    @Test
-    void givenDirTargetDirIsNotEmpty_whenCopyResourceFilesFlat_withCleanIfExists_thenTargetIsCleaned() throws IOException {
-        final File testDirectory = File.createTempFile("test-","");
-        // Delete temp file - we need a directory!
-        assertThat(testDirectory.delete()).isTrue();
-
-        try {
-            assertThat(testDirectory.mkdir()).isTrue();
-            final File testFile1 = File.createTempFile("test-",null, testDirectory);
-            final File testFile2 = File.createTempFile("test-",null, testDirectory);
-            int count = FileUtil.copyResourceFiles("testdata/file-util/subdir1", ".txt", testDirectory, true, true);
-            assertThat(count).isEqualTo(2);
-            assertThat(testDirectory.listFiles()).hasSize(2);
-            assertThat(testFile1.exists()).isFalse();
-            assertThat(testFile2.exists()).isFalse();
-        } finally {
-            FileUtils.deleteDirectory(testDirectory);
-        }
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "1-flat,testdata/file-util/subdir1,.txt,true,2,2,file2.txt",
-        "2-flat,testdata/file-util/subdir2,.txt,true,1,1,file1.txt",
-        "3-flat,testdata/file-util/subdir1,.json,true,1,1,file1.json",
-        "4-flat,testdata/file-util/,.txt,true,4,2,file2.txt",
-        "5-flat,testdata/file-util/,,true,6,3,file1.json",
-
-        "1-deep,testdata/file-util/subdir1,.txt,false,2,2,file2.txt",
-        "2-deep,testdata/file-util/subdir2,.txt,false,1,1,file1.txt",
-        "3-deep,testdata/file-util/subdir1,.json,false,1,1,file1.json",
-        "4-deep,testdata/file-util/,.txt,false,4,4,subdir2/file1.txt",
-        "5-deep,testdata/file-util/,,false,6,6,subdir2/file1.json",
-
-        "6,testdata/file-util/subdir1/simple,.xyz,true,0,0,",
-    })
-    void givenDirExistsWithFiles_whenCopyResourceFilesFlat_thenReturnNumberOfCopies(
-        String testId, String resourcePath, String endsWith, boolean flatten,
-        int expectedCopies, int expectedUnique, String expectedExampleFilename) throws IOException {
-
-        final File testDirectory = File.createTempFile("test-" + testId + "-","");
-        // Delete temp file - we need a directory!
-        assertThat(testDirectory.delete()).isTrue();
-        try {
-            int copied = FileUtil.copyResourceFiles(resourcePath, endsWith, testDirectory, true, flatten);
-            assertThat(copied).isEqualTo(expectedCopies);
-            if (copied > 0) {
-                // We test only one example file for existence
-                final File expectedFile = new File(testDirectory, expectedExampleFilename);
-                assertThat(expectedFile.exists()).isTrue();
-                assertThat(expectedFile.length()).isGreaterThan(0);
-                // count files recursively int the target directory
-                long fileCount = Files.find(testDirectory.toPath(), 4,
-                        (filePath, fileAttr) -> fileAttr.isRegularFile()).count();
-                assertThat(fileCount).isEqualTo(expectedUnique);
-            }
-        } finally {
-            FileUtils.deleteDirectory(testDirectory);
-        }
     }
 
     @Test
